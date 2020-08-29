61d92393dd2fd04dff52b73df2e8f0377842ca00
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2020-08-29 14:45:52 +0000

    Ajout de la variable speed en lecture seule

--- bicycle.dart
+++ bicycle.dart
@@ -1,27 +1,20 @@
 //Definition de la classe
 class Bicycle {
   int cadence;
-  int speed;
+  int _speed = 0;
+  //Ajouter le getter
+  int get speed => _speed;
   int gear;
   //Definition du constructeur de la classe
-  Bicycle(this.cadence, this.speed, this.gear);
-  /* 
-  le code ci-dessus du contructeur est eauivalent à ce aui suit :
-
-    Bicycle(int cadence, int speed, int gear){
-      this.cadence = cadence;
-      this.speed =  speed;
-      this.gear = gear;
-    }
-  */
+  Bicycle(this.cadence, this.gear);
   //Ajout de toString()
   @override
-  String toString() => 'Bicycle: $speed mph';
+  String toString() => 'Bicycle: $_speed mph';
 }
 
 //Methode main()
 void main() {
   //Instancier et imprimer une inatance Bicycle
-  var bicyclette = Bicycle(2, 0, 1);
+  var bicyclette = Bicycle(2, 1);
   print(bicyclette);
 }
