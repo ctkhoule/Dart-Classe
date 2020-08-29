95ffd315033f34cd4d5521df508452776f96803c
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2020-08-29 14:51:53 +0000

    Terminer l'implementation de speed en en tant que variable d'instance en lecture seule

--- bicycle.dart
+++ bicycle.dart
@@ -5,8 +5,19 @@ class Bicycle {
   //Ajouter le getter
   int get speed => _speed;
   int gear;
+
   //Definition du constructeur de la classe
   Bicycle(this.cadence, this.gear);
+
+  //Terminer l'implementation de speed en en tant que variable d'instance en lecture seule
+  void applybrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
   //Ajout de toString()
   @override
   String toString() => 'Bicycle: $_speed mph';
